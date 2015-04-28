1c2f980d92ba3a1318594357b38ac3bc8860b39f
Antonius Harijanto <adennyh@gmail.com>
2015-04-28 03:14:21 -0700

    Working. Need to be tested still
    Comma is not a good delimited

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ def getHeader():
 def getText(parentElement, tag):
 	e = parentElement.find(tag)
 	if e is not None:
-		return e.text
+		return e.text if e.text is not None else ""
 	else:
 		return ""
 
@@ -36,6 +36,14 @@ def main():
 	# Print each of the value
 	for gv in em.findall("G_VESSEL"):
 		row = Row(getText(gv, "TRP_ID"), getText(gv, "TRP_ATA"), getText(gv, "TRP_PORT_FROM"), getText(gv, "VES_VESSEL_NAME"), getText(gv, "VES_VESSEL_FLAG"), getText(gv, "TRP_CURRENT_LOCATION"), getText(gv, "AGT_NAME"))
+		lga = gv.find("LIST_G_ACTIVITY")
+		for ga in lga.findall("G_ACTIVITY"):
+			row.addActivity("", getText(ga, "TYPE"), getText(ga, "START_TIME"), getText(ga, "END_TIME"), getText(ga, "ACT"), getText(ga, "LOCATION"))	
+
+		lgn = gv.find("LIST_G_NEXT_PORT")
+		for gn in lgn.findall("G_NEXT_PORT"):
+			row.addNextPort(getText(gn, "CLR_NEXT_PORT"))	
+
 		print row.dump() 
 
 def isLeaf(elem):
